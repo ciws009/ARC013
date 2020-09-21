e486b1e917e71538af30b2f7ebd057fdd2717d87
Shin <stariver0812@gmail.com>
2020-09-21 10:44:33 +0900

    revised

--- B.cpp
+++ B.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector> 
+#include <algorithm>
 #include <cmath>
 #include <map>
 #include <iomanip>
@@ -7,6 +8,25 @@ using namespace std;
 
 #define int long long
 
-signed main() {
+signed main() { 
+    int c;
+    cin >> c;
+    vector<vector<int>> v(c, vector<int> (3));
+    for(int i = 0; i < c; i++) cin >> v[i][0] >> v[i][1] >> v[i][2];
 
+    for(int i = 0; i < c; i++) sort(v[i].begin(), v[i].end());
+    
+    int n_max = 0;
+    int m_max = 0;
+    int l_max = 0;
+    for(int i = 0; i < c; i++) {
+        n_max = max(n_max, v[i][0]);
+        m_max = max(m_max, v[i][1]);
+        l_max = max(l_max, v[i][2]);
+    }
+
+    int volume = n_max * m_max * l_max;
+
+    cout << volume << endl;
+    return 0;
 }
